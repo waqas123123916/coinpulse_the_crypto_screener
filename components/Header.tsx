205650014265ad2b9c475d339950bdf57eda759d
waqas123123916 <waqas@cosmicsync.co>
2026-01-08 17:50:09 +0500

    fix logo path

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
     <header>
       <div className="main-container inner">
         <Link href={"/"}>
-          <Image src={"logo.svg"} alt="coinpuls logo" width={132} height={40} />
+          <Image src={"/logo.svg"} alt="coinpuls logo" width={132} height={40} />
         </Link>
         <nav>
           <Link
